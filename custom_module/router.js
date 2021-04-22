70beb3d94aaf62dd774a4ef9c30936ce750640fe
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2021-04-22 22:16:34 +0700

    Finalizing Belajar NodeJS ExpressJS #19 Module Router

---
+++ custom_module/router.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const routers = express.Router();
+
+routers.get("/", (req, res) => {
+  res.send("request dengan method GET");
+});
+
+routers.post("/", (req, res) => {
+  res.send("request dengan method POST");
+});
+
+routers.put("/", (req, res) => {
+  res.send("request dengan method PUT");
+});
+
+routers.delete("/", (req, res) => {
+  res.send("request dengan method DELETE");
+});
+
+routers.get("/post/:id", (req, res) => {
+  res.send("artikel-" + req.params.id);
+});
+
+routers.get("/page-*", (req, res) => {
+  res.send("route: " + req.path);
+});
+
+module.exports = routers;
--- index.js
+++ index.js
@@ -2,29 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// app.get("/", (req, res) => {
-//   res.send("request dengan method GET");
-// });
-
-// app.post("/", (req, res) => {
-//   res.send("request dengan method POST");
-// });
-
-// app.put("/", (req, res) => {
-//   res.send("request dengan method PUT");
-// });
-
-// app.delete("/", (req, res) => {
-//   res.send("request dengan method DELETE");
-// });
-
-// app.get("/post/:id", (req, res) => {
-//   res.send("artikel-" + req.params.id);
-// });
-
-app.get("/page-*", (req, res) => {
-  res.send("route: " + req.path);
-});
+const routers = require("./custom_module/router");
+app.use(routers);
 
 app.listen(port, () =>
   console.log(`Server running at http://localhost:${port}`)
