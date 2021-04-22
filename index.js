05c24be777bdd20b3617355f3ff01ab3c51c1bf3
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2021-04-22 22:05:23 +0700

    Finalizing Belajar NodeJS ExpressJS #17 Routing Dinamis

--- index.js
+++ index.js
@@ -2,20 +2,24 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("request dengan method GET");
-});
+// app.get("/", (req, res) => {
+//   res.send("request dengan method GET");
+// });
 
-app.post("/", (req, res) => {
-  res.send("request dengan method POST");
-});
+// app.post("/", (req, res) => {
+//   res.send("request dengan method POST");
+// });
 
-app.put("/", (req, res) => {
-  res.send("request dengan method PUT");
-});
+// app.put("/", (req, res) => {
+//   res.send("request dengan method PUT");
+// });
 
-app.delete("/", (req, res) => {
-  res.send("request dengan method DELETE");
+// app.delete("/", (req, res) => {
+//   res.send("request dengan method DELETE");
+// });
+
+app.get("/post/:id", (req, res) => {
+  res.send("artikel-" + req.params.id);
 });
 
 app.listen(port, () =>
