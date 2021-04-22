eac5cbb27faa2498875f43690d54258ddba11c84
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2021-04-22 21:59:50 +0700

    Finalizing Belajar NodeJS ExpressJS #16 Routing Dasar

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!")); // <= tambahkan ini
+app.get("/", (req, res) => {
+  res.send("request dengan method GET");
+});
+
+app.post("/", (req, res) => {
+  res.send("request dengan method POST");
+});
+
+app.put("/", (req, res) => {
+  res.send("request dengan method PUT");
+});
+
+app.delete("/", (req, res) => {
+  res.send("request dengan method DELETE");
+});
 
 app.listen(port, () =>
   console.log(`Server running at http://localhost:${port}`)
