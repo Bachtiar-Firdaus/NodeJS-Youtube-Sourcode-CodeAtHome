5c4101d6a3086fecd1c0364148f62bf1e4560ad2
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2021-05-03 22:15:32 +0700

    Finalizing Belajar NodeJS ExpressJS #21 Membuat Middleware Sederhana || Menangani Log

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const app = express();
 const port = 3000;
 
 const routers = require("./custom_module/router");
+
+const log = (req, res, next) => {
+  console.log(Date.now() + " " + req.ip + " " + req.originalUrl);
+  next();
+};
+
+app.use(log);
+
 app.use(routers);
 
 app.listen(port, () =>
