e389fc2ff2f1c314520bfe553075953caf79d41e
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2021-05-03 22:21:28 +0700

    Belajar NodeJS ExpressJS #22 Membuat Middleware Sederhana || Menangani Routing 404

--- index.js
+++ index.js
@@ -13,6 +13,28 @@ app.use(log);
 
 app.use(routers);
 
+// middleware menangani 404
+// kode deklarasi routing
+//versi 1
+// app.use((req, res, next) => {
+//   res.status(404).send("resource tidak ditemukan");
+// });
+//versi 2
+// app.use((req, res, next) => {
+//   res.json({
+//     status: "error",
+//     message: "resource tidak ditemukan",
+//   });
+// });
+//versi 3
+const notFound = (req, res, next) => {
+  res.json({
+    status: "error",
+    message: "resource tidak ditemukan",
+  });
+};
+app.use(notFound);
+
 app.listen(port, () =>
   console.log(`Server running at http://localhost:${port}`)
 );
